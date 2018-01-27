1e22595600ab21fa6bf39e66159b04bffda95f64
Cole Wrampelmeier <wrampelm@american.edu>
2018-01-26 21:57:41 -0500

    platformer player code

--- Assets/PlayerPlatform.cs
+++ Assets/PlayerPlatform.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class PlayerPlatform : MonoBehaviour {
 
+    public Transform deathTransform;
+
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+
+    float horiz;
+    float vert;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Platforming();
 	}
+
+    void Platforming()
+    {
+
+        
+        horiz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+
+        //rb.AddForce(new Vector2(horiz,0) * speed);
+        rb.velocity = new Vector2(horiz*speed, Mathf.Clamp(rb.velocity.y, -12,15));
+        
+      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
 }
