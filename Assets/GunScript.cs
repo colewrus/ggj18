76e2515d99af46fe53acf575a31770299ecfc987
Kreston  Shirley <notserk@Krestons-MacBook-Pro.local>
2018-01-26 22:03:34 -0500

    Basic bullet hell mechanics

---
+++ Assets/GunScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+    public GameObject bullet;
+    public float timer;
+    public float tick;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(tick < timer){
+            tick += 1 * Time.deltaTime;
+        }
+        else{
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            tick = 0;
+        }
+
+        MoveSpawn(Random.Range(-11, 11));
+	}
+
+
+    void MoveSpawn(float randomNum){
+            transform.position = new Vector3(11.5f, randomNum, 0);
+        Debug.Log(Random.Range(-11, 11));
+    }
+}
