6ff6665d75c9e0b01e16259af3dbe695871940fb
Cole Wrampelmeier <wrampelm@american.edu>
2018-01-26 21:03:27 -0500

    pls send code

---
+++ Assets/bulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+
+    public enum BulletType {straight, explode};
+
+    public BulletType myType;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myType == BulletType.straight)
+        {
+            transform.Translate(Vector3.left * speed *   Time.deltaTime);
+        }
+	}
+}
