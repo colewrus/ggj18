81ea1c024d065499b8abf92d8d8c4c9b9cef87d6
Kreston Shirley <kreston@taglabs.org>
2018-01-27 19:37:26 -0500

    Added scoring text and collision detection.

---
+++ Assets/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController SharedInstance;
+
+    public Text scoreLabel;
+    private int packets = 10;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void DamagePlayer(int damage)
+    {
+        packets -= damage;
+        scoreLabel.text = "Packets: " + packets;
+    }
+}
--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -7,6 +7,9 @@ public class GunScript : MonoBehaviour {
     public float timer;
     public float tick;
 
+    public float startWait = 2.0f;
+    public int enemiesPerWave = 5;
+
 	// Use this for initialization
 	void Start () {
         
@@ -28,6 +31,32 @@ public class GunScript : MonoBehaviour {
 
     void MoveSpawn(float randomNum){
             transform.position = new Vector3(11.5f, randomNum, 0);
-        Debug.Log(Random.Range(-11, 11));
+        //Debug.Log(Random.Range(-11, 11));
     }
+
+    //IEnumerator SpawnEnemyWaves()
+    //{
+    //    yield return new WaitForSeconds(startWait);
+    //    while (true)
+    //    {
+    //        float waveType = Random.Range(0.0f, 10.0f);
+    //        for (int i = 0; i < enemiesPerWave; i++)
+    //        {
+    //            Vector3 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Camera.main.pixelHeight + 2, 0));
+    //            Vector3 topRight = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight + 2, 0));
+    //            Vector3 spawnPosition = new Vector3(Random.Range(topLeft.x, topRight.x), topLeft.y, 0);
+    //            Quaternion spawnRotation = Quaternion.Euler(0, 0, 180);
+    //            if (waveType >= 5.0f)
+    //            {
+    //                Instantiate(enemyType1, spawnPosition, spawnRotation);
+    //            }
+    //            else
+    //            {
+    //                Instantiate(enemyType2, spawnPosition, spawnRotation);
+    //            }
+    //            yield return new WaitForSeconds(spawnInterval);
+    //        }
+    //        yield return new WaitForSeconds(waveInterval);
+    //    }
+    //}
 }
